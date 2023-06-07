2f07d5729f477729be7fec7a3c675c2d98a8f084
MicheleFranciolapilla <michele.franciolapilla@gmail.com>
2023-06-07 12:19:24 +0900

    Redirect from index to main page

--- index.php
+++ index.php
@@ -3,11 +3,5 @@
     session_destroy_all();
     session_check_and_start();
     $_SESSION['new'] = true;
-
-    require_once __DIR__ . './php_fragments_and_partials/fragment_top.php';
-    require_once __DIR__ . './php_fragments_and_partials/fragment_mid_top.php';
-?>
-    <a href="./pages/main_page.php">Vai</a>
-<?php
-    require_once __DIR__ . './php_fragments_and_partials/fragment_bottom.php';
+    header("Location: ./pages/main_page.php")
 ?>
